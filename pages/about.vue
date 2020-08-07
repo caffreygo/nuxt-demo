ba3761495e1f22c9291c645e8d0c91afa4ced22b
Jinrui Chen <caffreygo@163.com>
2020-08-07 14:12:25 +0800

    temp

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    {{text}}
+    <el-button type="primary">按钮</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default {
+  data() {
+    return {
+      text: '123',
+    };
+  },
+  mounted() {
+    console.log(window); // Window {postMessage: ƒ, blur: ƒ, focus: ƒ, close: ƒ, frames: Window, …}
+  },
+};
+</script>
+
+<style>
+</style>
